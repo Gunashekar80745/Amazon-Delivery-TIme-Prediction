1df273d65e437c4c11c5ae1655c70231ba22c503
Gunashekar80745 <114819570+Gunashekar80745@users.noreply.github.com>
2025-10-07 18:21:49 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import pickle
+import traceback
+
+# Configure Streamlit page
+st.set_page_config(
+    page_title="🚚 Amazon Delivery Time Prediction",
+    page_icon="🚚",
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
+
+# [REST OF THE STREAMLIT CODE FROM MY PREVIOUS RESPONSE]
